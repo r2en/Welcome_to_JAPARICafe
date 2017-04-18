091b767bb783b034a42a94c21708b1554f99d334
xxxHAL <s12161te@gmail.com>
2017-04-18 12:28:40 +0900

    JapariCaffe01.py

---
+++ JapariCaffe01.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+import RPi.GPIO as GPIO
+from time import sleep
+import commands
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25,GPIO.IN)
+
+def main():
+    while True:
+        val = GPIO.input(25)
+        print GPIO.input(25)
+        if(val == True):
+            welcome_to_japari_caffe()
+        sleep(1)
+
+def welcome_to_japari_caffe():
+    mp3 = commands.getoutput("mpg321 -q q02.mp3")
+    sleep(5)
+
+if __name__ == '__main__':
+    main()
