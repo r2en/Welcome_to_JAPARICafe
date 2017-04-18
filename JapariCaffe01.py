fda4a29d9258a40e3af83e33aa45058d50243a27
xxxHAL <s12161te@gmail.com>
2017-04-18 18:39:04 +0900

    JapariCaffe01.py

--- JapariCaffe01.py
+++ JapariCaffe01.py
@@ -15,7 +15,8 @@ def main():
         sleep(1)
 
 def welcome_to_japari_caffe():
-    mp3 = commands.getoutput("mpg321 -q q02.mp3")
+    #mp3 = commands.getoutput("mpg321 -q q02.mp3")
+    mp4 = commands.getoutput("omxplayer -o local m.mp4")
     sleep(5)
 
 if __name__ == '__main__':
